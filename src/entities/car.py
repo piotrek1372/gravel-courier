8d1f13f2a25a7fcaaee4cd6af020456e606deed4
piotrek1372 <karasinskipiotrek13@gmail.com>
2026-09-18 18:54:12 +0200

    18.09.2026 updating car class

--- src/entities/car.py
+++ src/entities/car.py
@@ -1,13 +1,12 @@
 class Car:
     def __init__(self):
-        self.x = x
-        self.y = y
-        self.rotate_degree = rotate_degree
-        self.speed = speed
-        self.acc = acc
-        self.max_speed = max_speed
-        self.braking_force = braking_force
-        self.turning_speed = turning_speed
+        self.x = 500
+        self.y = 400
+        self.rotate_degree = 0
+        self.speed = 0
+        self.acc = 2
+        self.max_speed = 40
+        self.braking_force = 1
+        self.turning_speed = 1.1
 
     def update(self):
-        
