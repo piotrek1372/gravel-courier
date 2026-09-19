3e16a5dfc70f4ada95c714141625e4536d062634
piotrek1372 <karasinskipiotrek13@gmail.com>
2026-09-19 05:11:58 +0200

    19.09.2026 update

--- src/entities/car.py
+++ src/entities/car.py
@@ -1,9 +1,8 @@
 import pygame
-
+from pygame import Vector2
 class Car:
     def __init__(self):
-        self.x = 500
-        self.y = 400
+        self.pos = Vector2(450, 300)
         self.rotate_degree = 0
         self.speed = 0
         self.acc = 2
@@ -14,7 +13,7 @@ class Car:
     def update(self, dt, keys):
         if keys[pygame.K_w] or keys[pygame.K_UP]:
             self.speed = self.acc
-        self.x += self.speed * dt
+        self.pos[0] += self.speed * dt
 
     def draw(self, screen):
-        rect = pygame.draw.rect(screen, "red", (self.x, self.y, 100, 300))
+        rect = pygame.draw.rect(screen, "red", (self.pos[0], self.pos[1], 100, 300))
