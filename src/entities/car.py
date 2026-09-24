2e2dbf1ae8da377394e20e6eb2ea4c3af34f7783
piotrek1372 <karasinskipiotrek13@gmail.com>
2026-09-24 18:23:22 +0200

    24.09.2026 update

--- src/entities/car.py
+++ src/entities/car.py
@@ -32,5 +32,6 @@ class Car:
 
     def draw(self, screen):
         self.rotated_image = pygame.transform.rotate(self.image, self.angle)
-        self.rotated_rect = self.rotated_image.get_rect().centerx, self.rotated_image.get_rect().centery
+        self.rotated_rect = self.rotated_image.get_rect()
+        self.rotated_rect.center = self.rotated_image.get_rect().center
         screen.blit(self.rotated_image, self.pos)
