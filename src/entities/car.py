4f8ec687fb1c3221ef81a5c5adffe0ccc5bc339a
piotrek1372 <karasinskipiotrek13@gmail.com>
2026-09-24 17:56:17 +0200

    24.09.2026 update

--- src/entities/car.py
+++ src/entities/car.py
@@ -17,19 +17,19 @@ class Car:
 
     def update(self, dt, keys):
         if keys[pygame.K_w] or keys[pygame.K_UP]:
-            self.speed += self.acc
+            self.speed += self.acc * dt
         elif keys[pygame.K_s] or keys[pygame.K_DOWN]:
-            self.speed -= self.braking_force
+            self.speed -= self.braking_force * dt
         if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.angle -= self.turning_speed * dt
         elif keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.angle += self.turning_speed * dt
         self.direction_x = math.sin(self.angle)
         self.direction_y = -math.cos(self.angle)
-        self.pos[0] += self.direction_x * self.speed * dt
-        self.pos[1] += self.direction_y * self.speed * dt
+        self.pos[0] += self.direction_x * self.speed
+        self.pos[1] += self.direction_y * self.speed
 
 
     def draw(self, screen):
         self.rotated_image = pygame.transform.rotate(self.image, self.angle)
-        screen.blit(self.rotated_image, (self.pos[0], self.pos[1]) )
+        screen.blit(self.rotated_image, (self.rotated_image.get_rect().centerx, self.rotated_image.get_rect().centery))
